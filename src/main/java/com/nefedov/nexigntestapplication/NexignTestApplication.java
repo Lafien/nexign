f1734defae08044646b0cd96e11f65fe33a70965
lafien <lifaenka@list.ru>
2024-11-13 11:17:22 +0300

    initial commit

---
+++ src/main/java/com/nefedov/nexigntestapplication/NexignTestApplication.java
@@ -0,0 +1,13 @@
+package com.nefedov.nexigntestapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexignTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexignTestApplication.class, args);
+	}
+
+}
